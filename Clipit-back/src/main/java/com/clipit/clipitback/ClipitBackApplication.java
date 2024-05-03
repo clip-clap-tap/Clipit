040e53efc5407d9404109b075de7b3a376c72c3b
hyewon-p <hyewon.parkhw@gmail.com>
2024-05-03 14:17:24 +0900

    initial commit

---
+++ Clipit-back/src/main/java/com/clipit/clipitback/ClipitBackApplication.java
@@ -0,0 +1,13 @@
+package com.clipit.clipitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipitBackApplication.class, args);
+	}
+
+}
---
+++ Clipit-back/src/test/java/com/clipit/clipitback/ClipitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clipit.clipitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
