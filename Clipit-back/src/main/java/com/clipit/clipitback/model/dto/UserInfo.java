e65027bdcbecdb02f50b2ca17f623b3642e00b7d
Myeonghwa Kim <mhwazz@gmail.com>
2024-05-05 00:53:01 +0900

    feat: UserInfo, UserProfile DTO 추가

--- Clipit-back/src/main/java/com/clipit/clipitback/model/dto/UserInfo.java
+++ Clipit-back/src/main/java/com/clipit/clipitback/model/dto/UserInfo.java
@@ -1,4 +1,70 @@
 package com.clipit.clipitback.model.dto;
 
 public class UserInfo {
+
+	private String id;
+	private String password;
+	private String nickname;
+	private String createDate;
+	private String status;
+
+	public UserInfo() {
+		super();
+	}
+
+	public UserInfo(String id, String password, String nickname, String createDate, String status) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.nickname = nickname;
+		this.createDate = createDate;
+		this.status = status;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "UserInfo [id=" + id + ", password=" + password + ", nickname=" + nickname + ", createDate=" + createDate
+				+ ", status=" + status + "]";
+	}
+
 }
--- Clipit-back/src/main/java/com/clipit/clipitback/model/dto/UserProfile.java
+++ Clipit-back/src/main/java/com/clipit/clipitback/model/dto/UserProfile.java
@@ -1,4 +1,80 @@
 package com.clipit.clipitback.model.dto;
 
 public class UserProfile {
+
+	private String id;
+	private String gender;
+	private float height;
+	private float weight;
+	private int age;
+	private String goal;
+
+	public UserProfile() {
+		super();
+	}
+
+	public UserProfile(String id, String gender, float height, float weight, int age, String goal) {
+		super();
+		this.id = id;
+		this.gender = gender;
+		this.height = height;
+		this.weight = weight;
+		this.age = age;
+		this.goal = goal;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGoal() {
+		return goal;
+	}
+
+	public void setGoal(String goal) {
+		this.goal = goal;
+	}
+
+	@Override
+	public String toString() {
+		return "UserProfile [id=" + id + ", gender=" + gender + ", height=" + height + ", weight=" + weight + ", age="
+				+ age + ", goal=" + goal + "]";
+	}
+
 }
