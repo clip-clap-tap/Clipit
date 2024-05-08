64d7734ebe141fe0f9dc0ca0000c9f8c5d74537a
Myeonghwa Kim <mhwazz@hanmail.net>
2024-05-09 08:50:06 +0900

    feat: 유저별 전체 포스트 목록 확인 수정

--- Clipit-back/src/main/java/com/clipit/clipitback/controller/UserController.java
+++ Clipit-back/src/main/java/com/clipit/clipitback/controller/UserController.java
@@ -80,22 +80,22 @@ public class UserController {
 
 	@Operation(summary="찜한 포스트 목록 확인")
 	@GetMapping("/{id}/favorite-posts")
-	public ResponseEntity<?> getFavoritePostByUserId(String id){
+	public ResponseEntity<?> getFavoritePostsByUserId(String id){
 		List<Post> list = postService.getFavoritePostsByUserId(id);
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 
 	@Operation(summary="게시한 포스트 목록 확인")
 	@GetMapping("/{id}/posts")
-	public ResponseEntity<?> getPostByUserId(String id){
+	public ResponseEntity<?> getWrittenPostsByUserId(String id){
 		List<Post> list = postService.getPostsByWriterId(id);
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 
 	@Operation(summary="유저별 전체 목록 확인")
 	@GetMapping("/{id}/all-posts")
-	public ResponseEntity<?> getAllPostsById(String id){
-		List<Post> list = postService.getPostsByWriterId(id);
+	public ResponseEntity<?> getFavoriteOrWrittenPostsById(String id){
+		List<Post> list = postService.getAllPostsById(id);
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 
