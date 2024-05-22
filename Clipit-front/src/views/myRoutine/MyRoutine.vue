624a31a617aebd35466f45453f9b4b6b19e0b7dc
Myeonghwa Kim <mhwazz@hanmail.net>
2024-05-22 17:21:16 +0900

    feat: 마이루틴에 필터 기능 추가

--- Clipit-front/src/views/myRoutine/MyRoutine.vue
+++ Clipit-front/src/views/myRoutine/MyRoutine.vue
@@ -8,11 +8,26 @@ const store = usePostStore();
 onMounted(() => {
     store.getUserPosts();
 });
+
+const getAll = () => {
+    store.getUserPosts();
+};
+
+const getMine = () => {
+    store.getMine();
+};
+
+const getFav = () => {
+    store.getFav();
+};
 </script>
 <template>
     <div class="w-full flex flex-col items-center max-w-4xl gap-3">
         <div class="flex justify-between w-full px-4 items-center">
-            <span class="text-xl font-bold">my routines</span>
+            <span class="text-xl font-bold">My Routines</span>
+            <Button severity="info" outlined @click="getAll">전체</Button>
+            <Button severity="info" outlined @click="getMine">내가 작성</Button>
+            <Button severity="info" outlined @click="getFav">찜</Button>
             <RouterLink :to="{ name: `newPost` }">
                 <Button severity="info" outlined>추가</Button>
             </RouterLink>
