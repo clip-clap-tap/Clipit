5c6d66c36bdd28035c48655b1fffe74ec272c437
hyewon-p <hyewon.parkhw@gmail.com>
2024-05-22 17:41:15 +0900

    fix: layout 수정

--- Clipit-front/src/views/myRoutine/MyRoutine.vue
+++ Clipit-front/src/views/myRoutine/MyRoutine.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { usePostStore } from '@/stores/PostStore';
 import Button from 'primevue/button';
+import { ref } from 'vue';
 import { onMounted } from 'vue';
 
 const store = usePostStore();
@@ -11,23 +12,46 @@ onMounted(() => {
 
 const getAll = () => {
     store.getUserPosts();
+    current.value = 'all';
 };
 
 const getMine = () => {
     store.getMine();
+    current.value = 'mine';
 };
 
 const getFav = () => {
     store.getFav();
+    current.value = 'fav';
 };
+
+const current = ref('all');
 </script>
 <template>
     <div class="w-full flex flex-col items-center max-w-4xl gap-3">
         <div class="flex justify-between w-full px-4 items-center">
             <span class="text-xl font-bold">My Routines</span>
-            <Button severity="info" outlined @click="getAll">전체</Button>
-            <Button severity="info" outlined @click="getMine">내가 작성</Button>
-            <Button severity="info" outlined @click="getFav">찜</Button>
+            <div class="grow"></div>
+            <div class="flex mr-5">
+                <Button
+                    :class="`${current == 'all' ? 'underline' : ''} underline-offset-4`"
+                    @click="getAll"
+                    text
+                    >All</Button
+                >
+                <Button
+                    :class="`${current == 'mine' ? 'underline' : ''} underline-offset-4`"
+                    @click="getMine"
+                    text
+                    >My</Button
+                >
+                <Button
+                    :class="`${current == 'fav' ? 'underline' : ''} underline-offset-4`"
+                    @click="getFav"
+                    text
+                    >Favorite</Button
+                >
+            </div>
             <RouterLink :to="{ name: `newPost` }">
                 <Button severity="info" outlined>추가</Button>
             </RouterLink>
